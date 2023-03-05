1c38e5a8e54d324433f43e6e46364b500449a47f
Junaed <junaed1995@gmail.com>
2023-03-05 21:08:52 +0600

    Initial Commit

---
+++ Barcode-Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode-Scanner
+//
+//  Created by Junaed Muhammad Chowdhury on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
